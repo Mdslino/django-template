d91d58fc2ac963e1f2391f8602a533bcca7ccfe2
Marcelo Lino <mdslino@gmail.com>
2025-06-22 13:29:07 -0300

    Corrigir rota do teste de healthcheck para '/health/' no arquivo test_healthcheck.py

--- tests/test_views/test_healthcheck.py
+++ tests/test_views/test_healthcheck.py
@@ -3,6 +3,6 @@ import pytest
 
 @pytest.mark.django_db
 def test_healthcheck(client):
-    response = client.get('/ht/', headers={'Accept': 'application/json'})
+    response = client.get('/health/', headers={'Accept': 'application/json'})
     assert response.status_code == 200
     assert response.json() == {'DatabaseBackend': 'working'}
